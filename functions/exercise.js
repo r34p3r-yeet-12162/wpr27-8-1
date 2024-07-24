9de78dcefa9dfa81ef7155d19257bee66a09d4cf
r34p3r-yeet-12162 <tmkwaira@outlook.com>
2024-07-24 16:11:31 +0200

    Create exercise.js

---
+++ functions/exercise.js
@@ -0,0 +1,14 @@
+/* 
+
+    create a program that uses functions to find prime numbers within a range
+
+    FindPrimes(2, 20) => 2,3,5,7,11,13,17,19
+    FindPrimes(2, 23) => 2,3,5,7,11,13,17,19, 23
+    
+    // Modify the function so that you return an an object. See the example below
+    // suppose we call FindPrimes(2,10)
+    {
+    primes: [2,3,5,7],
+    composite: [4,6,8,9,10]
+    }
+*/
