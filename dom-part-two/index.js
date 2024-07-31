186c990a458c36dbb63c02f7a0b2654fd6f339b5
r34p3r-yeet-12162 <tmkwaira@outlook.com>
2024-07-31 15:50:44 +0200

    dom-part-b

--- dom-part-two/index.js
+++ dom-part-two/index.js
@@ -13,14 +13,14 @@ function ReadInput(inputId) {
   //
   //alert(recipeName);
   if (recipeName === "") {
+    document.getElementById(inputId).style.border = "2px solid red";
+    //  document.getElementById(inputId+ '-error').style.border = "2px solid red";
+    //
     error = true;
-    return;
   }
-  // what if there is an error?
   if (error) {
-    alert("error");
+    alert(`The input has an error`);
   }
-  document.getElementById(inputId).style.border = "2px solid red";
 
   //
   return recipeName;
@@ -39,7 +39,6 @@ function PrintRecipeToOutputParagraph(recipe) {
 function AddRecipe() {
   // read input
   const inputRecipe = ReadInput("recipe-name");
-  alert(inputRecipe);
 
   // guard clauses
   /* if (!inputRecipe)
@@ -50,4 +49,60 @@ function AddRecipe() {
   return !inputRecipe ? false : PrintRecipeToOutputParagraph(inputRecipe);
 }
 
-//console.log(ReadInput());
+function CreateForm() {
+  alert("creating form");
+  // Create elements by passing the tag name as an argument to the .createElement method
+  /* STEP 1: CREATE THE NODES! */
+
+  // Creating a FORM element
+  let formElement = document.createElement("form");
+
+  // a child of the FORM is label, with id: recipe type; and with text 'Recipe Name'
+  let recipeNameLabel = document.createElement("label");
+  recipeNameLabel.setAttribute("for", "recipe-name");
+  recipeNameLabel.setAttribute("id", "recipe-title");
+  recipeNameLabel.innerHTML = "Recipe Name";
+
+  // lets create the INPUT for the recipe name
+  let recipeNameInput = document.createElement("input");
+  recipeNameInput.setAttribute("id", "recipe-name");
+  recipeNameInput.setAttribute("placeholder", "enter a recipe");
+
+  // name error label
+  let recipeNameError = document.createElement("label");
+
+  recipeNameError.setAttribute("for", "recipe-name-error");
+  recipeNameError.setAttribute("class", "error"); // set to default at creation time
+  recipeNameError.innerHTML = "*";
+
+  //Button
+  let btnAddrecipeElem = document.createElement("button");
+  btnAddrecipeElem.setAttribute("type", "button");
+  btnAddrecipeElem.setAttribute("id", "recipe-name-error");
+  btnAddrecipeElem.innerHTML = "Add recipe";
+
+  //> adding an event listener for the button
+  btnAddrecipeElem.addEventListener("click", () => {
+    AddRecipe();
+  });
+
+  // horizontal line
+  let hrElem = document.createElement("hr");
+
+  // output paragraph
+  let paragraphElem = document.createElement("p");
+  paragraphElem.setAttribute("id", "output");
+  paragraphElem.innerHTML = "No recipes";
+
+  /* STEP 2: APPEND THEM TO THE DOCUMENT */
+  //
+
+  formElement.appendChild(recipeNameLabel);
+  formElement.appendChild(recipeNameInput);
+  formElement.appendChild(recipeNameError);
+  formElement.appendChild(btnAddrecipeElem);
+  formElement.appendChild(hrElem);
+  formElement.appendChild(paragraphElem);
+  //
+  document.body.appendChild(formElement);
+}
