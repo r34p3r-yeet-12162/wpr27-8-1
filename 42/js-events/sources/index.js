52b1998471b880e0e2ec573860d54d0b3ced24ac
r34p3r-yeet-12162 <tmkwaira@outlook.com>
2024-08-06 15:21:37 +0200

    More on events

---
+++ 42/js-events/sources/index.js
@@ -0,0 +1,68 @@
+/* 
+    Problem: We want to find odds and evens
+
+*/
+
+// The first problem with this function is that it' calculates odd or even' and then 'logs it to console'
+// This function is doing more that one thing!
+// What if we want to print this to:
+//  1. console
+//  2. text file
+//  3. DB object
+//  4. etc
+
+function isNumber(inputVal) {
+  if (isNaN(inputVal)) return false;
+  return inputVal;
+}
+
+// where do we get the 4 from? The 4 on the next line
+function readInput() {
+  // 1. prompt // to use prompt, we need to be on the frontend. Prompts are used to ask for input in a pop up
+  //let inputVal = prompt("Provide a number: "); //
+
+  /* suppose we have an input */
+  //
+  let inputValElem = document.getElementById("inputVal");
+  let inputVal = inputValElem.value;
+
+  if (!inputVal.length) return;
+  inputVal = Number(inputVal);
+  //alert(typeof inputVal); //2.alert is a modal popup that shows info
+  /* let res = 4;
+  return res; */
+  // for testing use altert, log, etc
+  alert(inputVal);
+
+  if (typeof inputVal === "number") {
+    console.log(inputVal);
+    return inputVal;
+  } else {
+    return;
+  }
+}
+
+// let inputVal = readInput(); //
+
+// let res = isNumber(inputVal);
+
+function findOddsandEvens(inputVal) {
+  if (typeof inputVal !== "number") return `Provide a number.`;
+
+  // let number = inputVal; // These values can come from anywhere
+  if (inputVal % 2 === 0) return `${inputVal} is even`; //
+  // console.log(`${inputVal} is even`); //
+  else return `${inputVal} is odd`; //
+
+  /* if(inputVal%2 === 0)
+  {
+    console.log(`${inputVal} is even`);
+  }
+  else{
+    console.log(`${inputVal} is odd`);
+
+
+  } */
+}
+
+// console.log(findOddsandEvens(res));
